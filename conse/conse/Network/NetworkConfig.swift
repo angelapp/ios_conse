d1845f7f0029809e6db0a77e896c2c63a072b3ab
Felipe Zamudio <okazakitomoyo729@gmail.com>
2018-01-04 21:12:56 -0500

    Network files

---
+++ conse/conse/Network/NetworkConfig.swift
@@ -0,0 +1,124 @@
+//
+//  NetworkConfig.swift
+//  conse
+//
+//  Created by Felipe Zamudio on 4/01/18.
+//  Copyright © 2018 NRC. All rights reserved.
+//
+
+import Alamofire
+import AlamofireObjectMapper
+import ObjectMapper
+import SwiftyJSON
+
+/// Se establecen las configuraciones básicas de Headers para los métodos POST, PUT y GET
+class Network: NSObject {
+    
+    /**
+     Config the request.
+     
+     Add header of content type, appi-id  and accept; the optional headers are authorization, DEVICEID.
+     If the request requires a body, it adds it
+     
+     - Parameter url: url of the enpoint api
+     - Parameter json: String in JSON format with the body data, if the request don't have body this field is *Empty*
+     - Parameter method: Rest Method (**GET**, **POST**, **PUT**)
+     - Parameter extraHeaders: Array with the optional headers
+     */
+    class func setupRequest(_ url: String, json: String!, extraHeaders: [[String: String]], method: NetworkRestMethods) -> URLRequest! {
+        
+        var request = URLRequest(url: URL(string: url)!)
+        printDebugMessage(tag: "\(request)")
+        
+        request.httpMethod = method.rawValue
+        request.setValue(NetworkConfig.apiKey, forHTTPHeaderField: NetworkConfig.headerApikey)
+        request.setValue(NetworkConfig.appJson, forHTTPHeaderField: NetworkConfig.headerAccept)
+        request.setValue(NetworkConfig.appJson, forHTTPHeaderField: NetworkConfig.headerContentType)
+        
+        for header in extraHeaders {
+            request.setValue(header[NetworkConfig.headerValue], forHTTPHeaderField: header[NetworkConfig.headerName]!)
+        }
+        
+        if json != nil && !json.isEmpty {
+            request.httpBody = json.data(using: String.Encoding(rawValue: String.Encoding.utf8.rawValue))
+            print(json!)
+        }
+        
+        return request
+    }
+    
+    /**
+     Crea url del api a consumir, y establece los parametros de configuración de la solicutud; si no hay error en la configuración
+     de la solucitud se envia al servidor
+     
+     - Parameter urlApi: EndPoint de la capacidad a consumir
+     - Parameter json: String en formato *JSON* con el cuerpo de la solicitud, si la solicitud no requiere de un cuerpo este valor debe ser vacio
+     - Parameter extraHeaders: Arreglo *clave : valor* con los encabezado y sus respectivos valores, si la solicitus no requiere de header adicionales, se debe enviar el arreglo vacio
+     - Parameter method: Metodo REST *(GET, POST, PUT)*
+     - Parameter completion: (callBack)
+     */
+    class func buildRequest (urlApi: String, json: String!, extraHeaders: [[String: String]], method: NetworkRestMethods, completion: @escaping (ResponseCallback) -> ()) {
+        
+        let url = [NetworkConfig.urlAccess, urlApi].flatMap{$0}.joined(separator: "")
+        
+        guard let request = setupRequest(url, json: json!, extraHeaders: extraHeaders, method: method) else {
+            completion(ResponseCallback.error(error: CustomError.NoData(description: NetworkErrorMessage.createRequestError)))
+            return
+        }
+        
+        sendRequest(request: request, completion: completion)
+    }
+    
+    /**
+     Función para procesar el request, y retornar la respuesta por medio del callback
+     
+     - Parameter request: *URLRequest* con la configuración de *Headers*, *Parameters* y *EndPoints*
+     - Parameter completion: Callback
+     */
+    class func sendRequest (request: URLRequest, completion: @escaping (ResponseCallback) -> ()) {
+        
+        Alamofire.request(request).responseJSON { response in
+            
+            guard response.response != nil else {
+                completion(ResponseCallback.succeeded(succeed: false, message: NetworkErrorMessage.notConexionError))
+                return
+            }
+            
+            guard let data = response.data, let _ = String(data: data, encoding: .utf8) else {
+                completion(ResponseCallback.error(error: CustomError.NoData(description: NetworkErrorMessage.parsingResultError)))
+                return
+            }
+            
+            guard let resp = response.response else {
+                completion(ResponseCallback.error(error: CustomError.NoData(description: NetworkErrorMessage.nilResponseError)))
+                return
+            }
+            
+            printDebugMessage(tag: String(resp.statusCode))
+            
+            let JSONResponse = response.result.value
+            
+            var result: Bool = false
+            var message = nullString
+            
+            switch resp.statusCode {
+            case NetworkCodes.successful:
+                result = true
+                completion(ResponseCallback.succeededObject(result: JSONResponse as AnyObject))
+                break
+                
+            case NetworkCodes.internalServerError:
+                result = false
+                message = NetworkErrorMessage.internalServerError
+                completion(ResponseCallback.succeeded(succeed: result, message: message))
+                break
+                
+            default:
+                result = false
+                message =  NetworkErrorMessage.defaultRequestError
+                completion(ResponseCallback.succeeded(succeed: result, message: message))
+                break
+            }
+        }
+    }
+}
---
+++ conse/conse/Network/NetworkConstants.swift
@@ -0,0 +1,97 @@
+//
+//  NetworkConstants.swift
+//  conse
+//
+//  Created by Felipe Zamudio on 4/01/18.
+//  Copyright © 2018 NRC. All rights reserved.
+//
+
+import Foundation
+
+enum ResponseCallback {
+    case succeeded(succeed: Bool, message: String)
+    case succeededObject(result: AnyObject)
+    case succeededArray(result: Array<AnyObject>)
+    case error(error: Error?)
+}
+
+enum CustomError:Error {
+    case NoData(description:String)
+}
+
+enum NetworkRestMethods: String {
+    case methodGET = "GET"
+    case methodPOST = "POST"
+    case methodPUT = "PUT"
+}
+
+/// Status Response Code
+struct NetworkCodes {
+    static let internalServerError = 500
+    static let successful = 200
+    static let notFound = 404
+}
+
+/// Strings para configurar los Headers
+struct NetworkConfig {
+    //Root Api url
+    #if DEBUG
+        static let urlAccess = "http://consejo.nrc.org.co:9000/api/"
+    #else
+        static let urlAccess = "http://consejo.nrc.org.co:9000/api/"
+    #endif
+    
+    //Header
+    static let headerAccept = "Accept"
+    static let headerApikey = "Api-Key"
+    static let headerAuthorization = "Authorization"
+    static let headerContentType = "Content-Type"
+    
+    //Header's values
+    static let appJson = "application/json"
+    static let token = "Token "
+    
+    #if DEBUG
+        static let apiKey = "3036c87d7f906cc2b8155b7a70428eb008de81aa"
+    #else
+        static let apiKey = "3036c87d7f906cc2b8155b7a70428eb008de81aa"
+    #endif
+    
+    //Key
+    static let headerName = "header"
+    static let headerValue = "value"
+}
+
+/// Mensajes de error
+struct NetworkErrorMessage {
+    static let createRequestError = "Error creating request"
+    static let defaultRequestError = "Failed Request!"
+    static let internalServerError = "Parece que hubo algún problema, por favor intentalo más tarde o comunicate con soporte"
+    static let msnDefaultError = "¡Comprueba tu conexión a internet he inténtalo de nuevo!"
+    static let nilResponseError = "Error response is nil"
+    static let notConexionError = "Reviza tu conexión a internet he inténtalo de nuevo"
+    static let notFoundError = "Parece que hubo algún problema, comunicate con nuestras lineas de atención"
+    static let parsingResultError = "Error parsing result"
+}
+
+/// String para establecer el 'EndPoint' de los métodos POST
+struct NetworkPOST {
+    static let USER_PROFILE = "create_user/"
+    static let CONTACT_FORM = "contact_form/"
+    static let AVATAR_LIST = "user_avatar/"
+    static let USER_PROGRESS_LIST = "user_progress/"
+    static let USER_LOGGIN = "user_auth/"
+    static let PASSWORD_RECOVERY = "user_password_recovery/"
+}
+
+/// String para establecer el 'EndPoint' de los métodos GET
+struct NetworkGET {
+    static let APP_CONFIGURATION = "applicationConfiguration/1/"
+    static let LIBRARY_DOCS = "get_library_docs/"
+    static let SHIELDS = "get_corporate_phone_book"
+}
+
+/// String of the 'EndPoint' to PUT Method
+struct NetworkPUT {
+    static let USER_PROFILE_EDIT = "profile/"
+}
